0f2630ef952855c903ee631a4745f9f9f87f6ee2
paragdutta7575 <paragdutt06@gmail.com>
2026-07-22 00:35:22 +0530

    Initial commit: calculator program

---
+++ Conditionals.py
@@ -0,0 +1,7 @@
+day_of_week = input ("Enter the day of the week: ").lower()
+print (day_of_week) 
+
+if day_of_week == "saturday" or day_of_week == "sunday":
+    print ("Yes this is when we need to work on skills")
+else:
+    print ("Its a usual day")
---
+++ Hello.py
@@ -0,0 +1,2 @@
+name = input ("Please confirm your name")
+print ("Hello - Good Going lets do it", name )
