d54b1b3f776a2e6f5fd0c8535291743b9aa6daf9
paragdutta7575 <paragdutt06@gmail.com>
2026-08-27 00:44:39 +0530

    We add script using dic

---
+++ Dict.py
@@ -0,0 +1,28 @@
+def cpu_check(server_cpu):
+    healthy = 0
+    unhealthy = 0
+
+    for server_name, cpu in server_cpu.items():
+        if cpu >= 80:
+            unhealthy += 1
+            print(server_name, cpu)
+        elif cpu >= 60:
+           print(server_name, cpu)
+           unhealthy += 1
+        else :
+           print ("all good")
+           healthy = 0
+ 
+server_cpu = {
+    "web-1": 45,
+    "web-2": 92,
+    "web-3": 78,
+    "web-4": 88,
+    "db-1": 15,
+    "db-2": 99,
+    "cache-1": 60
+}
+
+cpu_check(server_cpu)
+
+    
