17d796ba7be77e035f858f3a96a6fc8524279502
paragdutta7575 <paragdutt06@gmail.com>
2026-08-27 23:14:32 +0530

    Adding error and IP regex

--- Reggex.py
+++ Reggex.py
@@ -1,11 +1,8 @@
 
 import re
 
-log_data = """
-Error from 192.168.1.10
-Warning from 10.0.0.5
-Connection reset by 172.16.0.2
-"""
-
-find_ip = re.findall(r"\d+\.\d+\.\d+\.\d+", log_data)
-print(find_ip)
+with open ("Logs.log") as f:
+     for line in f:
+        match = re.search(r"\w+\.\w+\.\w+\.\w+", line)
+        if match:
+            print("Found Error", match.group())
